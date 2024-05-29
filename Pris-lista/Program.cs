aa34ad4b4382a5bdcc40c6941f4146eb032a7453
ammarovehub <ammarove1@pm.me>
2024-05-29 05:27:07 +0200

    Adda huvud menysval

--- Pris-lista/Program.cs
+++ Pris-lista/Program.cs
@@ -8,3 +8,11 @@ class Program
 
         while (keepRunning)
         {
+            // Visa huvudmenyn
+            Console.WriteLine("\nVälkommen till huvudmenyn!");
+            Console.WriteLine("1. Ungdom eller pensionär");
+            Console.WriteLine("2. Gruppbiljett");
+            Console.WriteLine("3. Upprepa text tio gånger");
+            Console.WriteLine("4. Det tredje ordet");
+            Console.WriteLine("0. Avsluta");
+            Console.Write("Ange ditt val: ");
